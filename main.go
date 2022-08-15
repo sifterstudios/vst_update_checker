4aecf55b37e2e23e96f7d05fb09520e629ca7f83
Tormod Liseth <tormod@sifterstudios.no>
2022-08-15 22:56:55 +0200

    Reading file as text

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	content, err := os.ReadFile("C:/Users/thesifter/AppData/Roaming/Steinberg/Cubase 12_64/Cubase Pro VST3 Cache/vst3plugins.xml")
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) //
 	}
 
 	println(string(content))

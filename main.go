bf4ce38d49428e3d61bb97a1344841889d458fec
Tormod Liseth <tormod@sifterstudios.no>
2022-08-15 23:38:29 +0200

    Added structs for xml parsing

--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"encoding/xml"
 	"log"
 	"os"
 )
 
+type Plugins struct {
+	XMLName xml.Name `xml:"plugins"`
+	Plugins []Plugin `xml:"plugin"`
+}
+
+type Timestamps struct {
+	XMLName    xml.Name `xml:"timestamps"`
+	Executable string   `xml:"executable"`
+}
+
+type Class struct {
+	XMLName       xml.Name `xml:"class"`
+	Cid           string   `xml:"cid"`
+	Cardinality   int      `xml:"cardinality"`
+	Category      string   `xml:"category"`
+	Name          string   `xml:"name"`
+	ClassFlags    int      `xml:"classFlags"`
+	SubCategories string   `xml:"subCategories"`
+	Vendor        string   `xml:"vendor"`
+	Version       string   `xml:"version"`
+	SdkVersion    string   `xml:"sdkVersion"`
+}
+
+type Plugin struct {
+	XMLName    xml.Name   `xml:"plugin"`
+	Path       string     `xml:"path"`
+	Vendor     string     `xml:"vendor"`
+	Url        string     `xml:"url"`
+	Email      string     `xml:"email"`
+	Flags      int        `xml:"flags"`
+	Codesigned bool       `xml:"codesigned"`
+	Timestamps Timestamps `xml:"timestamps"`
+	Class      Class      `xml:"class"`
+}
+
 func main() {
-	content, err := os.ReadFile("C:/Users/thesifter/AppData/Roaming/Steinberg/Cubase 12_64/Cubase Pro VST3 Cache/vst3plugins.xml")
+	xmlFile, err := os.Open("C:/Users/thesifter/AppData/Roaming/Steinberg/Cubase 12_64/Cubase Pro VST3 Cache/vst3plugins.xml")
 
 	if err != nil {
 		log.Fatal(err) //
 	}
 
-	println(string(content))
+	println("Successfully Opened 'vst3plugins.xml'")
+	defer xmlFile.Close()
 }
